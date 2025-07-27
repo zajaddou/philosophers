0c06103d02626abb4a36b043dedd6b0dccf2e72e
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-07-27 20:27:31 +0100

    update

--- philo/functions/philo_life.c
+++ philo/functions/philo_life.c
@@ -6,7 +6,7 @@
 /*   By: zajaddou <zajaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 15:48:42 by zajaddou          #+#    #+#             */
-/*   Updated: 2025/07/27 19:35:03 by zajaddou         ###   ########.fr       */
+/*   Updated: 2025/07/27 20:27:11 by zajaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,11 @@ void	*philo_life(void *arg)
 		lock(philo->r_fork);
 		print_status(philo, "has taken a fork");
 		print_status(philo, "is eating");
+		run(time_eat(GET));
 		lock(&philo->safe_monitor);
 		philo->last_eat = get_time();
 		philo->eat_num++;
 		unlock(&philo->safe_monitor);
-		run(time_eat(GET));
 		unlock(philo->l_fork);
 		unlock(philo->r_fork);
 		print_status(philo, "is sleeping");
