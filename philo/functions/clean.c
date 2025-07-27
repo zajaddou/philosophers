bfc3d938500ad08213bcab2b3cade1d01c9efd14
Zakaria Ajaddou <54480895+zajaddou@users.noreply.github.com>
2025-07-27 21:02:02 +0100

    update

--- philo/functions/clean.c
+++ philo/functions/clean.c
@@ -6,7 +6,7 @@
 /*   By: zajaddou <zajaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 02:47:53 by zajaddou          #+#    #+#             */
-/*   Updated: 2025/07/27 20:21:33 by zajaddou         ###   ########.fr       */
+/*   Updated: 2025/07/27 20:47:44 by zajaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	clean_all(void)
 	t_philo	*philo;
 	int		i;
 
-	unlock(write_lock());
 	pthread_mutex_destroy(write_lock());
 	philo = philo_stack();
 	i = -1;
